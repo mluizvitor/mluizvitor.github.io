5e39436275cc5a40c80b641c4d5de25a9378d35b
Vitor Monteiro <mluizvitor@gmail.com>
2020-05-20 14:07:13 -0300

    feat: added "send mail" method

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {ContainerEnhanced} from '../components/Container';
 import { TextInput, TextAreaInput, Form } from '../components/TextInput';
 import contactBg from '../assets/backgroundContacts.svg';
 import Button from '../components/Button';
 
 export default function Contact({id}){
+
+  const [status, setStatus] = useState();
+
+  function handleSubmitMessage(event){
+    event.preventDefault();
+
+    const form = event.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        setStatus("SUCCESS");
+      } else {
+        setStatus("ERROR");
+      }
+    };
+    xhr.send(data);
+  }
+
   return (
     <ContainerEnhanced id={id} type="container" backgroundImage={contactBg} >
       <h2>Contato</h2>
-      <Form>
+      <Form
+        method="POST"
+        action="https://formspree.io/mvowzvdr"
+        onSubmit={handleSubmitMessage}
+      >
         <TextInput id="name" label="Nome" placeholder="João da Silva" required/>
         <TextInput id="email" label="Email" placeholder="joao@mail.com" required/>
         <TextAreaInput id="message" label="Mensagem" placeholder="Olá, Vitor..." required/>
-        <div
-          class="g-recaptcha"
-          data-sitekey="6LcKxtEUAAAAADJpiahRzDjvgNGywQlrtTi-6gj5"
-        ></div>
-        <Button title="Enviar"/>
+        {status === "ERROR" && <p className="subtitle formStatus error">Ooops! Aconteceu um erro.</p>}
+        {status === "SUCCESS" && <p className="subtitle formStatus success">Mensagem enviada. Obrigado!</p>}
+          <Button title="Enviar" type="submit"/>
       </Form>
     </ContainerEnhanced>
   )
