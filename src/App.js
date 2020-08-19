07b4bf3f5da9694ba69d5704707f758655f8f462
Luiz Vitor Monteiro <mluizvitor@gmail.com>
2020-08-19 10:36:45 -0300

    chore: rearranged code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Link} from 'react-scroll';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyle from './styles/global';
 
 import Navbar from './components/Navbar';
 import WhoIam from './pages/WhoIam';
@@ -9,34 +11,17 @@ import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 
 function App() {
-
-  const App = {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  };
   return (
-    <div style={App}>
-      <Navbar>
-        <Link to="who-i-am" activeClass="active" className="button" spy={true} smooth={true} offset={-64} duration={500}>
-          Quem é vitor?
-        </Link>
-        <Link to="skills" activeClass="active" className="button" spy={true} smooth={true} offset={-64} duration={500}>
-          Habilidades
-        </Link>
-        <Link to="portfolio" activeClass="active" className="button" spy={true} smooth={true} offset={-64} duration={500}>
-          Portfólio
-        </Link>
-        <Link to="contact" activeClass="active" className="button" spy={true} smooth={true} offset={-64} duration={500}>
-          Contato
-        </Link>
-      </Navbar>
-      <WhoIam id="who-i-am"/>
-      <Skills id="skills"/>
-      <Portfolio id="portfolio"/>
-      <Contact id="contact"/>
-      <Footer/>
-    </div>
+    <>
+      <BrowserRouter>{/* <Routes/> */}</BrowserRouter>
+      <Navbar />
+      <WhoIam id="who-i-am" />
+      <Skills id="skills" />
+      <Portfolio id="portfolio" />
+      <Contact id="contact" />
+      <Footer />
+      <GlobalStyle />
+    </>
   );
 }
 
