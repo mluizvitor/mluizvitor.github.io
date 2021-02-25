dee0384972c59c4ad9f3f7861f60da020cb81ebb
Vitor Monteiro <mluizvitor@gmail.com>
2021-02-25 13:53:05 -0300

    chore: added color variables

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+import {lighten} from 'polished';
+
+export const theme = {
+  t001: {
+    f: "#1A1C28ff",
+    a: "#1A1C2800",
+  },
+  t002: {
+    f: "#5C3472ff",
+    a: "#5C347200",
+  },
+  t003: "#cc3d94",
+  t004: "#DE6896",
+  t005: "#ED97A6",
+  t006: lighten(0.2, "#ED97A6")
+}
