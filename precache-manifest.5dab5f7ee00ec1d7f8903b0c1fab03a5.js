f54fc772d30381e679b67480798f5b96b93ba48a
Vitor Monteiro <mluizvitor@gmail.com>
2022-02-17 19:24:44 -0300

    Updates

---
+++ precache-manifest.5dab5f7ee00ec1d7f8903b0c1fab03a5.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2440de6bf7c1eae440a0c685f17728e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45ab968ec3f35bfec35f",
+    "url": "/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "45ab968ec3f35bfec35f",
+    "url": "/static/js/2.12e6695a.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.12e6695a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b859c8b5d5aaa03cce7b",
+    "url": "/static/js/main.4d778ba8.chunk.js"
+  },
+  {
+    "revision": "fba6a1a1da2eb3b64423",
+    "url": "/static/js/runtime-main.dbcddcbf.js"
+  },
+  {
+    "revision": "28110157f3f848a4b50c3cb8425e8c1c",
+    "url": "/static/media/avatar.28110157.svg"
+  },
+  {
+    "revision": "fcbe511ad14e5209c484d06ea8d6bdde",
+    "url": "/static/media/background.fcbe511a.webp"
+  },
+  {
+    "revision": "3aece1f6f9f9d223d318f676b0cf7fae",
+    "url": "/static/media/backgroundContacts.3aece1f6.svg"
+  },
+  {
+    "revision": "a46d356dfc7e626a7490919ad0ea9918",
+    "url": "/static/media/backgroundHeader.a46d356d.svg"
+  },
+  {
+    "revision": "c74fbd4fd08556ac8cca1a386967e21b",
+    "url": "/static/media/cover.c74fbd4f.webp"
+  },
+  {
+    "revision": "adea28f9d91e52f0fbef6b54f374cf6f",
+    "url": "/static/media/css3.adea28f9.svg"
+  },
+  {
+    "revision": "f26b0fa7eb617c4e5fa1ce20d53a1291",
+    "url": "/static/media/desktop1.f26b0fa7.webp"
+  },
+  {
+    "revision": "0599e650748e8f0dacaafb6834de9d5d",
+    "url": "/static/media/desktop2.0599e650.webp"
+  },
+  {
+    "revision": "fa3473bd54e98f76659c66d39d6e7fee",
+    "url": "/static/media/desktop3.fa3473bd.webp"
+  },
+  {
+    "revision": "c4979dc454d995f25d064410bf25922b",
+    "url": "/static/media/docs1.c4979dc4.webp"
+  },
+  {
+    "revision": "cc4803f11466e3e78bb841eddff572df",
+    "url": "/static/media/docs2.cc4803f1.webp"
+  },
+  {
+    "revision": "da46f49938c62ffbe70025d22c41f7e0",
+    "url": "/static/media/docs3.da46f499.webp"
+  },
+  {
+    "revision": "a970de21a9f6301ed477f0909b31f2bd",
+    "url": "/static/media/elegumes.a970de21.jpg"
+  },
+  {
+    "revision": "32888b643a88d7541c4e15020cc3dd06",
+    "url": "/static/media/esquelestore.32888b64.jpg"
+  },
+  {
+    "revision": "ddd8b35da0a92459d91eafc33ba30288",
+    "url": "/static/media/figma.ddd8b35d.svg"
+  },
+  {
+    "revision": "77fa20227fe52597073660462fb0621b",
+    "url": "/static/media/git.77fa2022.svg"
+  },
+  {
+    "revision": "ca9dec48f0d1df1eb3c38e964544b7df",
+    "url": "/static/media/html5.ca9dec48.svg"
+  },
+  {
+    "revision": "fc2199a04086d491b6a5b05828a6a2ec",
+    "url": "/static/media/inkscape.fc2199a0.svg"
+  },
+  {
+    "revision": "b4e19cce832209a1114e08c3cd89f6ce",
+    "url": "/static/media/js.b4e19cce.svg"
+  },
+  {
+    "revision": "da6088021614bd64c2c42047f11055e1",
+    "url": "/static/media/logo.da608802.svg"
+  },
+  {
+    "revision": "cbd63d0846b6877de2fd321c8ee8abb8",
+    "url": "/static/media/material.cbd63d08.svg"
+  },
+  {
+    "revision": "1250459f55bbc1ac53d1178dec080c85",
+    "url": "/static/media/mobile0.1250459f.webp"
+  },
+  {
+    "revision": "a931774abb89a776d66be5c042a10ae5",
+    "url": "/static/media/mobilevid1.a931774a.mp4"
+  },
+  {
+    "revision": "c38d6b05d72905a91462a4b05c48e013",
+    "url": "/static/media/mobilevid2.c38d6b05.mp4"
+  },
+  {
+    "revision": "0d967d134e6731719e337e9b3a6475c1",
+    "url": "/static/media/mobilevid3.0d967d13.mp4"
+  },
+  {
+    "revision": "12ad433d1a91a1cffd140cd34e9af6fb",
+    "url": "/static/media/node.12ad433d.svg"
+  },
+  {
+    "revision": "638b6a50775503584685a9d130a0fcba",
+    "url": "/static/media/poraque.638b6a50.jpg"
+  },
+  {
+    "revision": "85de898668d0fdf2d9fca501aee634ed",
+    "url": "/static/media/profile.85de8986.webp"
+  },
+  {
+    "revision": "3d09669090d1f293e0b6c8440c60ded2",
+    "url": "/static/media/react.3d096690.svg"
+  },
+  {
+    "revision": "679afaa2a760fc643eb321c26d284c4c",
+    "url": "/static/media/uxpin.679afaa2.svg"
+  }
+]);
